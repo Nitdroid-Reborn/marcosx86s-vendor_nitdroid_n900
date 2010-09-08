782c482693a3f0a61c4b83e5a81d754ba2836402
Alexey Roslyakov <alexey.roslyakov@newsycat.com>
2010-09-08 17:44:58 +0400

    Removed unneded files: init.sh and load_modules.sh

--- load_modules.sh
+++
@@ -1,7 +0,0 @@
-#!/system/bin/sh
-BDADDR="0cddef0a0e6d"
-swapon /dev/block/mmcblk1p3
-echo 25 > /proc/vm/swappiness
-chmod 0777 /dev/pvrsrvkm
-chown system.graphics /dev/pvrsrvkm
-echo $BDADDR | sed 's/\(..\)/\1:/g;s/:$//' > /sys/devices/platform/hci_h4p/bdaddr
