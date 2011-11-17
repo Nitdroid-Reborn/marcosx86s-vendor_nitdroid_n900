c855a9363bd998d4164100dcef056bb209534de8
Alexey Roslyakov <alexey.roslyakov@newsycat.com>
2011-11-18 02:33:43 +0400

    Use PRODUCT_COPY_FILES instead of ALL_PREBUILT

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -12,123 +12,33 @@ LOCAL_MODULE_TAGS := optional
 include $(BUILD_KEY_CHAR_MAP)
 include $(CLEAR_VARS)
 
-file := $(TARGET_OUT_KEYLAYOUT)/omap_twl4030keypad.kl
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/omap_twl4030keypad.kl | $(ACP)
-	$(transform-prebuilt-to-target)
+#
+#
+#
 
-file := $(TARGET_OUT_KEYLAYOUT)/triton2-pwrbutton.kl
-LOCAL_MODULE_TAGS := optional
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/triton2-pwrbutton.kl | $(ACP)
-	$(transform-prebuilt-to-target)
-
-file := $(TARGET_OUT_KEYLAYOUT)/gpio-keys.kl
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/gpio-keys.kl | $(ACP)
-	$(transform-prebuilt-to-target)
-
-file := $(TARGET_ROOT_OUT)/bin/busybox
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/busybox/busybox | $(ACP)
-	$(transform-prebuilt-to-target)
-
-file := $(TARGET_ROOT_OUT)/init.rc
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/init.rc | $(ACP)
-	$(transform-prebuilt-to-target)
-
-file := $(TARGET_ROOT_OUT)/init.nokia.rc
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/init.nokia.rc | $(ACP)
-	$(transform-prebuilt-to-target)
-
-file := $(TARGET_ROOT_OUT)/system/etc/vold.fstab
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/etc/vold.fstab | $(ACP)
-	$(transform-prebuilt-to-target)
-
-file := $(TARGET_ROOT_OUT)/system/etc/switchswap.sh
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/etc/switchswap.sh | $(ACP)
-	$(transform-prebuilt-to-target)
-
-file := $(TARGET_ROOT_OUT)/system/xbin/rr
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/system/xbin/rr | $(ACP)
-	$(transform-prebuilt-to-target)
-
-file := $(TARGET_ROOT_OUT)/system/etc/modem.conf
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/etc/modem.conf | $(ACP)
-	$(transform-prebuilt-to-target)
-
-file := $(TARGET_ROOT_OUT)/system/etc/gps.conf
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/etc/gps.conf | $(ACP)
-	$(transform-prebuilt-to-target)
-
-file := $(TARGET_ROOT_OUT)/system/etc/asound.conf
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/etc/asound.conf | $(ACP)
-	$(transform-prebuilt-to-target)
-
-file := $(TARGET_ROOT_OUT)/system/etc/init.nokia.sh
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/etc/init.nokia.sh | $(ACP)
-	$(transform-prebuilt-to-target)
-
-file := $(TARGET_ROOT_OUT)/ueventd.nokia.rc
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/ueventd.nokia.rc | $(ACP)
-	$(transform-prebuilt-to-target)
-
-file := $(TARGET_ROOT_OUT)/system/etc/dhcpcd/dhcpcd.conf
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/etc/dhcpcd/dhcpcd.conf | $(ACP)
-	$(transform-prebuilt-to-target)
-
-file := $(TARGET_ROOT_OUT)/system/etc/wifi/wpa_supplicant.conf
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/etc/wifi/wpa_supplicant.conf | $(ACP)
-	$(transform-prebuilt-to-target)
-
-file := $(TARGET_ROOT_OUT)/system/maemo/bin/bme
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/system/maemo/bin/bme | $(ACP)
-	$(transform-prebuilt-to-target)
-
-file := $(TARGET_ROOT_OUT)/system/maemo/bin/bq27
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/system/maemo/bin/bq27 | $(ACP)
-	$(transform-prebuilt-to-target)
-
-file := $(TARGET_ROOT_OUT)/system/maemo/bin/dsme
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/system/maemo/bin/dsme | $(ACP)
-	$(transform-prebuilt-to-target)
-
-file := $(TARGET_ROOT_OUT)/system/maemo/bin/init_subsys
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/system/maemo/bin/init_subsys | $(ACP)
-	$(transform-prebuilt-to-target)
-
-file := $(TARGET_ROOT_OUT)/system/maemo/bin/klogd
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/system/maemo/bin/klogd | $(ACP)
-	$(transform-prebuilt-to-target)
-
-file := $(TARGET_ROOT_OUT)/system/maemo/etc/android.sh
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/system/maemo/etc/android.sh | $(ACP)
-	$(transform-prebuilt-to-target)
-
-file := $(TARGET_ROOT_OUT)/system/maemo/etc/config.sh
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/system/maemo/etc/config.sh | $(ACP)
-	$(transform-prebuilt-to-target)
-
-file := $(TARGET_ROOT_OUT)/system/maemo/bin/wifi
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/system/maemo/bin/wifi | $(ACP)
-	$(transform-prebuilt-to-target)
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/omap_twl4030keypad.kl:$(TARGET_OUT_KEYLAYOUT)/omap_twl4030keypad.kl \
+	$(LOCAL_PATH)/triton2-pwrbutton.kl:$(TARGET_OUT_KEYLAYOUT)/triton2-pwrbutton.kl \
+	$(LOCAL_PATH)/gpio-keys.kl:$(TARGET_OUT_KEYLAYOUT)/gpio-keys.kl \
+	$(LOCAL_PATH)/busybox/busybox:$(TARGET_ROOT_OUT)/bin/busybox \
+	$(LOCAL_PATH)/init.rc:$(TARGET_ROOT_OUT)/init.rc \
+	$(LOCAL_PATH)/init.nokia.rc:$(TARGET_ROOT_OUT)/init.nokia.rc \
+	$(LOCAL_PATH)/etc/vold.fstab:$(LOCAL_PATH)/etc/vold.fstab \
+	$(LOCAL_PATH)/etc/switchswap.sh:$(TARGET_ROOT_OUT)/system/etc/switchswap.sh \
+	$(LOCAL_PATH)/system/xbin/rr:$(TARGET_ROOT_OUT)/system/xbin/rr \
+	$(LOCAL_PATH)/etc/modem.conf:$(TARGET_ROOT_OUT)/system/etc/modem.conf \
+	$(LOCAL_PATH)/etc/gps.conf:$(TARGET_ROOT_OUT)/system/etc/gps.conf \
+	$(LOCAL_PATH)/etc/asound.conf:$(TARGET_ROOT_OUT)/system/etc/asound.conf \
+	$(LOCAL_PATH)/etc/init.nokia.sh:$(TARGET_ROOT_OUT)/system/etc/init.nokia.sh \
+	$(LOCAL_PATH)/ueventd.nokia.rc:$(TARGET_ROOT_OUT)/ueventd.nokia.rc \
+	$(LOCAL_PATH)/etc/dhcpcd/dhcpcd.conf:$(TARGET_ROOT_OUT)/system/etc/dhcpcd/dhcpcd.conf \
+	$(LOCAL_PATH)/etc/wifi/wpa_supplicant.conf:$(TARGET_ROOT_OUT)/system/etc/wifi/wpa_supplicant.conf \
+	$(LOCAL_PATH)/system/maemo/bin/bme:$(TARGET_ROOT_OUT)/system/maemo/bin/bme \
+	$(LOCAL_PATH)/system/maemo/bin/bq27:$(TARGET_ROOT_OUT)/system/maemo/bin/bq27 \
+	$(LOCAL_PATH)/system/maemo/bin/dsme:$(TARGET_ROOT_OUT)/system/maemo/bin/dsme \
+	$(LOCAL_PATH)/system/maemo/bin/init_subsys:$(TARGET_ROOT_OUT)/system/maemo/bin/init_subsys \
+	$(LOCAL_PATH)/system/maemo/bin/klogd:$(TARGET_ROOT_OUT)/system/maemo/bin/klogd \
+	$(LOCAL_PATH)/system/maemo/etc/android.sh:$(TARGET_ROOT_OUT)/system/maemo/etc/android.sh \
+	$(LOCAL_PATH)/system/maemo/etc/config.sh:$(TARGET_ROOT_OUT)/system/maemo/etc/config.sh \
+	$(LOCAL_PATH)/system/maemo/bin/wifi:$(TARGET_ROOT_OUT)/system/maemo/bin/wifi
+##
