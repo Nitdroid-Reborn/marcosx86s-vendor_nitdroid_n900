f93dc3200384bc66f678c030c9569f15f00796f6
Alexey Roslyakov <alexey.roslyakov@newsycat.com>
2011-11-25 01:52:38 +0400

    Inherit product from full_base. Add device overlay

--- n900.mk
+++ n900.mk
@@ -2,7 +2,9 @@
 # It includes the base Android platform. If you need Google-specific features,
 # you should derive from generic_with_google.mk
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/generic.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+DEVICE_PACKAGE_OVERLAYS := vendor/nitdroid/n900/overlay
 
 # This is the list of apps included in the generic AOSP build
 PRODUCT_PACKAGES += \
